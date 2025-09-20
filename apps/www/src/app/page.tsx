519523ef3216bbbd32cc60d46d6503c477c193bf
aliimam-in <imam30114@gmail.com>
2025-09-20 23:08:33 +0530

    mjs

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,5 +1,4 @@
-import { ModeToggle } from "@/components/layout/mode-toggle";
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/layout/mode-toggle"; 
 import Image from "next/image";
 
 export default function Home() {
