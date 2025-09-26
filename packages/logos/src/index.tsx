b7d4cc3404e5b8a7bfbb65d2cb4a25f72608ad27
aliimam-in <imam30114@gmail.com>
2025-09-26 21:42:51 +0530

    fvsaf

---
+++ packages/logos/src/index.tsx
@@ -0,0 +1,8 @@
+export { default as ArrowLeft } from "./ArrowLeft.js";
+export { default as ArrowRight } from "./ArrowRight.js";
+export { default as ArrowUp } from "./ArrowUp.js";
+export { default as ArrowDown } from "./ArrowDown.js";
+export { default as Cursor } from "./Cursor.js";
+export { default as OpenAi } from "./OpenAi.js";
+export { default as Vercel } from "./Vercel.js";
+export { default as Google } from "./Google.js";
