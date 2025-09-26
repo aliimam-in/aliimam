f4638edd26b608f07c1c6711ab158fe87bb5fa6b
aliimam-in <imam30114@gmail.com>
2025-09-27 01:09:59 +0530

    kkbj

--- packages/icons/src/CursorIcon.tsx
+++ packages/icons/src/CursorIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import type { IconProps } from "../types";
+import type { IconProps } from "./types.js";
 
 export const CursorIcon = React.forwardRef<SVGSVGElement, IconProps>(
   function CursorIcon({ color = "currentColor", ...props }, forwardedRef) {
