07224f168dcb4eb10114f43b6b719da4e72d1ac2
aliimam-in <imam30114@gmail.com>
2025-09-27 01:04:35 +0530

    sdva

--- packages/icons/src/index.tsx
+++
@@ -1,9 +0,0 @@
-// Icons
-export { default as ArrowLeftIcon } from "./ArrowLeftIcon";
-export { default as ArrowRightIcon } from "./ArrowRightIcon";
-export { default as ArrowUpIcon } from "./ArrowUpIcon";
-export { default as ArrowDownIcon } from "./ArrowDownIcon";
-export { default as CursorIcon } from "./CursorIcon";
-export { default as OpenAiIcon } from "./OpenAiIcon";
-export { default as VercelIcon } from "./VercelIcon";
-export { default as GoogleIcon } from "./GoogleIcon";
