d7d81f065b489c3704f6fd03b93dc24c5cd8dce8
aliimam-in <imam30114@gmail.com>
2025-09-26 21:21:58 +0530

    sdvv

--- packages/logos/src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as ArrowLeft } from "./ArrowLeft.js";
-export { default as ArrowRight } from "./ArrowRight.js";
-export { default as Cursor } from "./Cursor.js";
-export { default as OpenAi } from "./OpenAi.js";
