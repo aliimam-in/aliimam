ca67a5ecfc53eca399022978fccde16fc32fc169
aliimam-in <imam30114@gmail.com>
2025-09-27 16:11:35 +0530

    fvsadv

---
+++ packages/icons/src/index.tsx
@@ -0,0 +1,8 @@
+// entry.tsx.ejs - Simple re-export structure
+
+export * from "./stroke/arrows";
+export * from "./stroke/gg";
+export * from "./stroke/ali";
+export * from "./solid/design";
+
+export type { IconProps } from "./types";
---
+++ packages/icons/src/stroke/gg/index.ts
@@ -0,0 +1,4 @@
+export { default as F } from "./F";
+export { default as R } from "./R";
+export { default as T } from "./T";
+export { default as Y } from "./Y";
