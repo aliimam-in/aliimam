22ced32902c6cbc89334610e422d5ae311eeb3ca
aliimam-in <imam30114@gmail.com>
2025-09-27 10:39:05 +0530

    dvas

--- packages/icons/src/index.tsx
+++ packages/icons/src/index.tsx
@@ -1,6 +1,8 @@
 // entry.tsx.ejs - Simple re-export structure
 
 export * from "./icons/arrows";
+export * from "./icons/gg";
 export * from "./logos/design";
+export * from "./logos/ali";
 
 export type { IconProps } from "./types";
