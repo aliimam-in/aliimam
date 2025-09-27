d7e62f900e6a364abf19c53375ecfe3e1957e8d4
aliimam-in <imam30114@gmail.com>
2025-09-27 16:09:43 +0530

    dcd

--- packages/icons/src/index.tsx
+++
@@ -1,8 +0,0 @@
-// entry.tsx.ejs - Simple re-export structure
-
-export * from "./stroke/arrows";
-export * from "./stroke/gg";
-export * from "./stroke/ali";
-export * from "./solid/design";
-
-export type { IconProps } from "./types";
