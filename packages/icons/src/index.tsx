0aa7b42032f9393d8b8a054a803a2a06114b5940
aliimam-in <imam30114@gmail.com>
2025-09-27 10:09:16 +0530

    sv

--- packages/icons/src/index.tsx
+++ packages/icons/src/index.tsx
@@ -1,6 +1,6 @@
 // entry.tsx.ejs - Simple re-export structure
 
-export * from "./arrows";
-export * from "./ai";
+export * from "./icons/arrows";
+export * from "./logos/ai";
 
 export type { IconProps } from "./types";
