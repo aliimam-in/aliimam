6ae05793a7b27c9c6ea4e1d140cd8d6599addde6
aliimam-in <imam30114@gmail.com>
2026-01-06 15:11:53 +0530

    update

--- apps/www/src/app/design/test.tsx
+++
@@ -1,9 +0,0 @@
-import type { SVGProps } from "react";
-
-const CustomIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg {...props} viewBox="0 0 200 200">
-    <rect x="0" y="0" width="200" height="200" fill="none" stroke="#ffffff" stroke-width="2"   /><g transform="translate(1.4765256983395716,1.3339365013926567) scale(1,1)" ><g clip-path="url(#undefined-clip0_2068_915)"><path d="M0 10.8H12V24H24V0H12L0 10.8Z" fill="#ffffff"></path></g><defs><clipPath id="undefined-clip0_2068_915"><path fill="#fff" d="M0 0H24V24H0z"></path></clipPath></defs></g>
-  </svg>
-);
-
-export { CustomIcon };
